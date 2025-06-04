24f05abdc98d7bb588501ffe40313d2b7b8814fb
paularads86 <paularads86@gmail.com>
2025-06-04 20:55:25 +0000

    xadrezmestrefinal

--- xadrez.c
+++ xadrez.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+//mover a torre 
+void moverTorre (int casas){ 
+    if (casas > 0) {
+        printf("Direita\n");
+        moverTorre (casas - 1);
+    }
+}
+//mover bispo
+void moverBispo (int casas){
+    if (casas > 0) {
+        printf("Cima, Direita\n");
+        moverBispo(casas - 1);
+    }
+}
+
+//mover rainha
+void moverrainha (int casas){
+    if (casas > 0) {
+        printf("Baixo\n");
+        moverrainha(casas - 1);
+    }
+}
+
 int main(){
 int peca;
 int movimento = 1;
@@ -10,6 +33,7 @@ int i = 1;
  printf("2.Mover peça 'Bispo'\n");
  printf("3.Mover peça 'Rainha'\n");
  printf("4.Mover peça 'Cavalo'\n");
+ printf("5.Mover peça 'Bispo' com loops alinhados\n");
  printf("Esolha uma peça: "); /*solicitando a peça para jogar*/
  scanf("%d", &peca); /*escolhendo a peça */
 
@@ -21,52 +45,50 @@ int i = 1;
          case 1:
         printf("Você escolheu a Torre\n"); 
         //mover a torre cindo casas para a direita
-        while (i <=5)
-             {
-             printf("Direita\n");
-             i++;  /* code */
-             }
+        moverTorre(5);
         break;
         
         //mover o bispo
          case 2:
          printf("Você escolheu o Bispo\n"); 
-        //mover o Bispoe cinco casas para cima e direita
-        for ( i ; i <= 5; i++)
-             {
-             printf("Cima, Direita\n");
-             }
+        //mover o Bispo e cinco casas para cima e direita
+        moverBispo(5);
          break;
       
          //mover a rainha
          case 3:
          printf("Você escolheu a Rainha\n"); 
          //mover a rainha oito casas para baixo
-         do
-             {
-              printf("Baixo\n");
-             i++;
-             } while (i<=8);
+         moverrainha(8);
          break;
 
          //mover cavalo
          case 4:
          printf("Você escolheu o Cavalo\n"); 
-         //mover o cavalo duas vezes para baixo e uma vez para a esquerda
-         while (movimento--)
-         {
-            for (int j = 0; j < 2; j++) {
-                printf("Baixo\n"); /*imprima baixo duas vezes*/
+         //mover o cavalo duas vezes para cima e uma vez para a direita
+         
+            for (int i = 0, j = 4; i < j; i++, j--)
+            {
+                printf("Cima\n"); /*imprima cima duas vezes*/
             }
-            printf("Esquerda\n"); /*imprime esquerda uma vez*/
-         }
-         
-         
-         break;
-   
-   }
-   
-   
+            printf("Direita\n"); /*imprima direita uma vez*/
+        break;
+
+        //mover bispo com loops alinhados
+        case 5:
+        printf("Você escolheu o Bispo\n");
+        for (int i = 1; i <=5; i++) //loop externo
+        {
+            for (int j = 1; j <=1; j++)/*loop interno*/
+            {
+            printf("Direita\n"); 
+            }
+         printf("Cima\n");
+        }
+       }
+ 
+  
 return 0;
 }
+
 
